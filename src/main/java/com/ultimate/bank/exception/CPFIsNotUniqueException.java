9a4365a0040a7e776352ce792c964f58b279f871
ultr4 <cl.rch09@icloud.com>
2024-03-29 02:47:02 -0300

    refactor: change CPFIsNotUniqueException to IsNotUniqueException

--- src/main/java/com/ultimate/bank/exception/CPFIsNotUniqueException.java
+++
@@ -1,7 +0,0 @@
-package com.ultimate.bank.exception;
-
-public class CPFIsNotUniqueException extends RuntimeException {
-    public CPFIsNotUniqueException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/ultimate/bank/exception/IsNotUniqueException.java
@@ -0,0 +1,7 @@
+package com.ultimate.bank.exception;
+
+public class IsNotUniqueException extends RuntimeException {
+    public IsNotUniqueException(String message) {
+        super(message);
+    }
+}
