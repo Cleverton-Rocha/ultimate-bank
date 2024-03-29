b05cb9205fea8edb1927207aabd1e6a3424d60ae
ultr4 <cl.rch09@icloud.com>
2024-03-29 02:47:18 -0300

    refactor: change UserNotFoundException to NotFoundException

--- src/main/java/com/ultimate/bank/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ultimate.bank.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
