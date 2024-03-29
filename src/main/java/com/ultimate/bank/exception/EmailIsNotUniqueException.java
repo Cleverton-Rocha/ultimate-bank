2a1fe59cee12db695904742a057f658fd332e8a7
ultr4 <cl.rch09@icloud.com>
2024-03-29 02:45:40 -0300

    delete: delete EmailIsNotUniqueException

--- src/main/java/com/ultimate/bank/exception/EmailIsNotUniqueException.java
+++
@@ -1,7 +0,0 @@
-package com.ultimate.bank.exception;
-
-public class EmailIsNotUniqueException extends RuntimeException {
-    public EmailIsNotUniqueException(String message) {
-        super(message);
-    }
-}
