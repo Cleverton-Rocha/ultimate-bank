ca03c76287f2a4358f0a1dd44c649f003a3f0dc0
ultr4 <cl.rch09@icloud.com>
2024-03-29 02:45:00 -0300

    feat: add findByUserCPF method

--- src/main/java/com/ultimate/bank/repository/AccountRepository.java
+++ src/main/java/com/ultimate/bank/repository/AccountRepository.java
@@ -3,5 +3,8 @@ package com.ultimate.bank.repository;
 import com.ultimate.bank.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUserCPF(String CPF);
 }
