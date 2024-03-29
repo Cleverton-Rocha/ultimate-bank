2159687226ee5d8fff116d4cd0ef6d560b278d30
ultr4 <cl.rch09@icloud.com>
2024-03-28 22:06:14 -0300

    refactor: Delete findByHashedCPF method

--- src/main/java/com/ultimate/bank/repository/UserRepository.java
+++ src/main/java/com/ultimate/bank/repository/UserRepository.java
@@ -10,6 +10,4 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByCPF(String CPF);
-
-    Optional<User> findByHashedCPF(String hashedCPF);
 }
