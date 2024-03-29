b9eed81bda07b0b60cb543353a24aea1eb815665
ultr4 <cl.rch09@icloud.com>
2024-03-28 23:59:41 -0300

    feat: add updateUser method

--- src/main/java/com/ultimate/bank/service/UserService.java
+++ src/main/java/com/ultimate/bank/service/UserService.java
@@ -3,13 +3,18 @@ package com.ultimate.bank.service;
 import com.ultimate.bank.domain.User;
 import com.ultimate.bank.exception.CPFIsNotUniqueException;
 import com.ultimate.bank.exception.EmailIsNotUniqueException;
-import com.ultimate.bank.model.user.UserRequest;
-import com.ultimate.bank.model.user.UserResponse;
+import com.ultimate.bank.exception.UserNotFoundException;
+import com.ultimate.bank.model.user.CreateUserRequest;
+import com.ultimate.bank.model.user.CreateUserResponse;
+import com.ultimate.bank.model.user.UpdateUserRequest;
+import com.ultimate.bank.model.user.UpdateUserResponse;
 import com.ultimate.bank.repository.UserRepository;
 import com.ultimate.bank.util.HashUtil;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,7 +29,7 @@ public class UserService {
     }
 
     @Transactional
-    public ResponseEntity<UserResponse> createUser(UserRequest request) {
+    public ResponseEntity<CreateUserResponse> createUser(CreateUserRequest request) {
 
         String hashedCPF = HashUtil.hashCPF(request.CPF());
 
@@ -42,11 +47,53 @@ public class UserService {
 
         userRepository.save(newUser);
 
-        return ResponseEntity.ok(new UserResponse(
+        return ResponseEntity.ok(new CreateUserResponse(
+                newUser.getId(),
                 newUser.getCPF(),
                 newUser.getName(),
                 newUser.getEmail(),
                 newUser.getPassword()));
+    }
+
+    @Transactional
+    public ResponseEntity<UpdateUserResponse> updateUser(Long id,
+                                                         UpdateUserRequest request,
+                                                         JwtAuthenticationToken token) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found."));
+
+        if (user.getCPF().equals(token.getName())) {
+            if (request.CPF() != null) {
+                String hashedCPF = HashUtil.hashCPF(request.CPF());
+                if (!hashedCPF.equals(user.getCPF()) &&
+                        userRepository.findByCPF(hashedCPF).isPresent()) {
+                    throw new CPFIsNotUniqueException("CPF already exists.");
+                }
+                user.setCPF(hashedCPF);
+            }
+
+            if (request.name() != null) {
+                user.setName(request.name());
+            }
+
+            if (request.email() != null) {
+                if (!request.email().equals(user.getEmail()) &&
+                        userRepository.findByEmail(request.email()).isPresent()) {
+                    throw new EmailIsNotUniqueException("Email already exists.");
+                }
+                user.setEmail(request.email());
+            }
+
+            if (request.password() != null) {
+                user.setPassword(passwordEncoder.encode(request.password()));
+            }
+
+            userRepository.save(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+        return ResponseEntity.ok(new UpdateUserResponse(user));
     }
 
 }
