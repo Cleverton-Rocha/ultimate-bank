6ed724770e78b8d89b0ddac1a5fbfb618fbffe2e
ultr4 <cl.rch09@icloud.com>
2024-03-29 01:00:46 -0300

    feat: add account property to UserResponse

--- src/main/java/com/ultimate/bank/model/user/UserResponse.java
+++ src/main/java/com/ultimate/bank/model/user/UserResponse.java
@@ -1,9 +1,10 @@
 package com.ultimate.bank.model.user;
 
+import com.ultimate.bank.domain.Account;
 import com.ultimate.bank.domain.User;
 
-public record UserResponse(Long id, String name, String email) {
+public record UserResponse(Long id, String name, String email, Account account) {
     public UserResponse(User user){
-        this(user.getId(), user.getName(), user.getEmail());
+        this(user.getId(), user.getName(), user.getEmail(), user.getAccount());
     }
 }
