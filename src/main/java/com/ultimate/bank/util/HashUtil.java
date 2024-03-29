7ec6e670f0a4a94035c3c8f1388fd140b8d76aa5
ultr4 <cl.rch09@icloud.com>
2024-03-28 22:23:54 -0300

    feat: create HashUtil class with hashCPF method

---
+++ src/main/java/com/ultimate/bank/util/HashUtil.java
@@ -0,0 +1,13 @@
+package com.ultimate.bank.util;
+
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
+
+public class HashUtil {
+    public static String hashCPF(String CPF) {
+        return Hashing.sha256()
+                .hashString(CPF, StandardCharsets.UTF_8)
+                .toString();
+    }
+}
