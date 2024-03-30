948d9d6a1eb470f7a98935641c5f9884ccd0da72
ultr4 <cl.rch09@icloud.com>
2024-03-29 22:38:02 -0300

    feat: add JwtAuthenticationToken

--- src/main/java/com/ultimate/bank/controller/AccountController.java
+++ src/main/java/com/ultimate/bank/controller/AccountController.java
@@ -2,6 +2,8 @@ package com.ultimate.bank.controller;
 
 import com.ultimate.bank.model.account.OperationRequest;
 import com.ultimate.bank.service.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,8 @@ public class AccountController {
     }
 
     @PostMapping("/transaction")
-    public void transaction(@RequestBody OperationRequest request) {
-        accountService.operation(request);
+    public void transaction(@RequestBody @Valid OperationRequest request,
+                            JwtAuthenticationToken token) {
+        accountService.operation(request, token);
     }
 }
