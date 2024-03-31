3049c677eb4ae70d8d1645736b31acac119873ab
ultr4 <cl.rch09@icloud.com>
2024-03-30 21:14:28 -0300

    refactor: add hashedCPF

--- src/main/java/com/ultimate/bank/model/auth/LoginResponse.java
+++ src/main/java/com/ultimate/bank/model/auth/LoginResponse.java
@@ -1,4 +1,4 @@
 package com.ultimate.bank.model.auth;
 
-public record LoginResponse(String accessToken, Long expiresIn) {
+public record LoginResponse(String accessToken, String hashedCPF, Long expiresIn) {
 }
