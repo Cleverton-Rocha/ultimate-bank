474183b2dd7f99adfa7939d6b1a1c159736efccd
ultr4 <cl.rch09@icloud.com>
2024-04-03 23:00:24 -0300

    refactor: remove password property from UpdateUserRequest

--- src/main/java/com/ultimate/bank/model/user/UpdateUserRequest.java
+++ src/main/java/com/ultimate/bank/model/user/UpdateUserRequest.java
@@ -13,10 +13,5 @@ public record UpdateUserRequest(
         @Size(min = 1, message = "Last name must have at least 1 character.")
         String lastName,
         @Email(message = "Write a valid email.")
-        String email,
-
-        @Size(min = 8, max = 100, message = "Password must have at least 8 characters.")
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$",
-                 message = "Password must have at least one uppercase letter, one lowercase letter and one number.")
-        String password) {
+        String email) {
 }
